74e126a4e8a7771e018be0c7bac2ef725fb86d64
Mateusz Czapliński <czapkofan@gmail.com>
2014-06-06 00:30:32 +0200

    watch only requested file

--- draq.go
+++ draq.go
@@ -49,14 +49,48 @@ func run() error {
 
 func watch(watcher *fsnotify.Watcher, fn string, q chan<- struct{}) {
 	defer func() { q <- struct{}{} }()
+
+	repaint := make(chan struct{}, 1)
+	// FIXME: handle errors in painter()
+	go painter(fn, repaint)
+	repaint <- struct{}{}
+
 	for {
 		select {
 		case ev := <-watcher.Event:
 			log.Println("event:", ev)
+			if ev.IsDelete() || ev.IsRename() {
+				break
+			}
+
+			// try to check if that's the watched file
+			f1, err := os.Stat(fn)
+			if err != nil {
+				break
+			}
+			f2, err := os.Stat(ev.Name)
+			if err != nil {
+				break
+			}
+			if !os.SameFile(f1, f2) {
+				break
+			}
+
+			select { // raise signal that repaint is needed, unless already queued
+			case repaint <- struct{}{}:
+			default:
+			}
 		case err := <-watcher.Error:
 			log.Println("error:", err)
 			watcher.Close()
 			return
 		}
+	}
+}
+
+func painter(fn string, signal <-chan struct{}) {
+	for {
+		<-signal
+		log.Println("repaint!")
 	}
 }
